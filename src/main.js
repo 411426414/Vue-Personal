0347e27e77657659a1dcbff2de056c87082c8f3d
411426414 <411426414@qq.com>
2020-12-07 23:40:40 +0800

    向main.js添加全局样式

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import 'element-ui/lib/theme-chalk/index.css'
 import Vue from 'vue'
 import App from './App.vue'
+import './assets/css/global.css'
 import './plugins/element.js'
 import router from './router'
 import store from './store'
