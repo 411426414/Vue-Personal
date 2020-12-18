19af64820c9df307eae8fa4bc6e15b623bf10aa5
411426414 <411426414@qq.com>
2020-12-18 22:06:25 +0800

    请求在到达服务器之前，先会调用use中的这个回调函数来添加请求头信息

--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
-import 'element-ui/lib/theme-chalk/index.css'\
+import 'element-ui/lib/theme-chalk/index.css'
 import Vue from 'vue'
 import App from './App.vue'
 import './assets/css/global.css'
@@ -11,6 +11,12 @@ Vue.config.productionTip = false
 
 // 配置请求根路径
 axios.defaults.baseURL = 'https://www.liulongbin.top:8888/api/private/v1/'
+
+// 在request 拦截器中, 展示进度条 NProgress.start()
+// 请求在到达服务器之前，先会调用use中的这个回调函数来添加请求头信息
+axios.interceptors.request.use(config => {
+  console.log(config)
+})
 
 // 挂在到Vue实例，后面可通过this调用
 Vue.prototype.$http = axios
