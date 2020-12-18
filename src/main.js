364730d0f3565bb90474efcca61ca0faef08de74
411426414 <411426414@qq.com>
2020-12-18 22:05:07 +0800

    引入axios

--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
-import 'element-ui/lib/theme-chalk/index.css'
+import axios from 'axios'
+import 'element-ui/lib/theme-chalk/index.css'\
 import Vue from 'vue'
 import App from './App.vue'
 import './assets/css/global.css'
@@ -8,6 +9,11 @@ import store from './store'
 
 Vue.config.productionTip = false
 
+// 配置请求根路径
+axios.defaults.baseURL = 'https://www.liulongbin.top:8888/api/private/v1/'
+
+// 挂在到Vue实例，后面可通过this调用
+Vue.prototype.$http = axios
 new Vue({
   router,
   store,
