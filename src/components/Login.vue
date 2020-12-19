06ee91a432b4cb33f48de0738ad5d7c298f3c1f6
411426414 <411426414@qq.com>
2020-12-19 23:25:47 +0800

    表单数据验证对象

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -50,13 +50,13 @@ export default {
         username: "admin",
         password: "123456"
       },
-      loginFormRules:{
-        username:[
-          { required: true, message: "请输入用户名", trigger: "blur"},
+      loginFormRules: {
+        username: [
+          { required: true, message: "请输入用户名", trigger: "blur" },
           { min: 3, max: 10, message: "长度在 3 到 10 个字符", trigger: "blur" }
-        ], 
-        password:[
-          { required: true, message: "请输入密码", trigger: "blur"},
+        ],
+        password: [
+          { required: true, message: "请输入密码", trigger: "blur" },
           { min: 6, max: 15, message: "长度在 6 到 15 个字符",  trigger: "blur" }
         ]
       }
