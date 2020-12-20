e642cd242e566a1d9b5da7545fa0c99468bdb0cf
411426414 <411426414@qq.com>
2020-12-20 21:32:39 +0800

    添加element-ui组件

--- src/plugins/element.js
+++ src/plugins/element.js
@@ -1,8 +1,13 @@
 import {
+  Aside,
   Button,
+  Container,
+  Footer,
   Form,
   FormItem,
+  Header,
   Input,
+  Main,
   Message
 } from 'element-ui'
 import Vue from 'vue'
@@ -10,7 +15,12 @@ import Vue from 'vue'
 Vue.use(Button)
 Vue.use(Form)
 Vue.use(FormItem)
+Vue.use(Footer)
 Vue.use(Input)
+Vue.use(Main)
+Vue.use(Header)
+Vue.use(Container)
+Vue.use(Aside)
 
 // 将弹框组件全局挂在到VUe原型实例
 Vue.prototype.$message = Message
