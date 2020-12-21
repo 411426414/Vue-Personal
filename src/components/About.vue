b48719ea4077226df2c69390dec1c2d21c9c2d17
411426414 <411426414@qq.com>
2020-12-21 23:44:29 +0800

    添加关于我的盒子容器

--- src/components/About.vue
+++ src/components/About.vue
@@ -1,6 +1,8 @@
 <template>
   <div class="about">
     <div class="img">关于我</div>
+    <div class="about-me">
+    </div>
   </div>
 </template>
 
@@ -16,5 +18,9 @@ export default {};
   width: 100%;
   height: 100vh;
   background: url(https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1608574050504&di=68f515bd3d85c220d756640c725fb8e2&imgtype=0&src=http%3A%2F%2Fbbsfiles.vivo.com.cn%2Fvivobbs%2Fattachment%2Fforum%2F201706%2F24%2F120513xjmo2xki377j4d9x.jpg);
+}
+.about-me {
+  width: 100%;
+  height: 810px;
 }
 </style>
