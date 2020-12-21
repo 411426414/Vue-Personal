7532a0992ab0e910c3ddadec356b327dd76010b4
411426414 <411426414@qq.com>
2020-12-21 23:21:58 +0800

    设置组件的宽高

--- src/components/About.vue
+++ src/components/About.vue
@@ -1,12 +1,14 @@
 <template>
-  <div>
-    关于我
-  </div>
+  <div class="about">关于我</div>
 </template>
 
 <script>
 export default {};
 </script>
 
-<style lang="scss" scoped>
+<style lang="less" scoped>
+.about {
+  width: 100vw;
+  height: 100vh;
+}
 </style>
