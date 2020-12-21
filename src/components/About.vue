d52f7e792391c7c65fd77edeee3fc351beb1cd4f
411426414 <411426414@qq.com>
2020-12-21 23:22:07 +0800

    设置背景图片

--- src/components/About.vue
+++ src/components/About.vue
@@ -10,5 +10,6 @@ export default {};
 .about {
   width: 100vw;
   height: 100vh;
+  background: url(https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1608574050504&di=68f515bd3d85c220d756640c725fb8e2&imgtype=0&src=http%3A%2F%2Fbbsfiles.vivo.com.cn%2Fvivobbs%2Fattachment%2Fforum%2F201706%2F24%2F120513xjmo2xki377j4d9x.jpg);
 }
 </style>
