48171580b6f202655bccc662453b53c82a3fd155
411426414 <411426414@qq.com>
2020-12-21 23:08:45 +0800

    添加关于组件路由

--- src/router/index.js
+++ src/router/index.js
@@ -18,6 +18,11 @@ const routes = [
     path: '/home',
     name: 'Home',
     component: () => import('@/views/Home')
+  },
+  {
+    path: '/about',
+    name: 'About',
+    component: () => import('@/components/About')
   }
 ]
 
