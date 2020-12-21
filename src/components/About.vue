b5c52208ec988be4ec6a317543e964b0fdf2f94d
411426414 <411426414@qq.com>
2020-12-21 23:43:45 +0800

    添加关于组件

--- src/components/About.vue
+++ src/components/About.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="about">关于我</div>
+  <div class="about"></div>
 </template>
 
 <script>
@@ -9,7 +9,5 @@ export default {};
 <style lang="less" scoped>
 .about {
   width: 100vw;
-  height: 100vh;
-  background: url(https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1608574050504&di=68f515bd3d85c220d756640c725fb8e2&imgtype=0&src=http%3A%2F%2Fbbsfiles.vivo.com.cn%2Fvivobbs%2Fattachment%2Fforum%2F201706%2F24%2F120513xjmo2xki377j4d9x.jpg);
 }
 </style>
