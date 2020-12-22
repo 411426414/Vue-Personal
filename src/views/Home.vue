df11fc69b5ce0f0ec8fe139890a7dd1ad94bfb82
411426414 <411426414@qq.com>
2020-12-22 21:33:12 +0800

    添加导航菜单

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,42 +1,104 @@
 <template>
-  <div>
+  <el-container class="container">
     <el-container>
       <el-header>Header</el-header>
       <el-container>
-        <el-aside width="200px">Aside</el-aside>
+        <el-aside width="200px">
+          <el-menu
+            text-color="#fff"
+            :collapse-transition="false"
+            class="el-menu-vertical-demo"
+            background-color="#545c64"
+            active-text-color="#ffd04b"
+          >
+            <el-submenu index="1">
+              <template slot="title">
+                <i class="el-icon-location"></i>
+                <span>导航一</span>
+              </template>
+              <el-menu-item-group>
+                <template slot="title">分组一</template>
+                <el-menu-item index="1-1">选项1</el-menu-item>
+                <el-menu-item index="1-2">选项2</el-menu-item>
+              </el-menu-item-group>
+              <el-menu-item-group title="分组2">
+                <el-menu-item index="1-3">选项3</el-menu-item>
+              </el-menu-item-group>
+              <el-submenu index="1-4">
+                <template slot="title">选项4</template>
+                <el-menu-item index="1-4-1">选项1</el-menu-item>
+              </el-submenu>
+            </el-submenu>
+            <el-menu-item index="/home">
+              <i class="el-icon-menu"></i>
+              <span slot="title">导航二</span>
+            </el-menu-item>
+            <el-menu-item index="3" disabled>
+              <i class="el-icon-document"></i>
+              <span slot="title">导航三</span>
+            </el-menu-item>
+            <el-menu-item index="/about">
+              <i class="el-icon-setting"></i>
+              <span slot="title">关于</span>
+            </el-menu-item>
+          </el-menu>
+        </el-aside>
         <el-container>
           <el-main>Main</el-main>
           <el-footer>Footer</el-footer>
         </el-container>
       </el-container>
     </el-container>
-  </div>
+  </el-container>
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {}
+  }
+}
 </script>
 
 <style lang="less" scoped>
-.el-header,
-.el-footer {
-  background-color: #b3c0d1;
-  color: #333;
-  text-align: center;
-  line-height: 60px;
+.container {
+  height: 100%;
+}
+
+.el-header {
+  background-color: #373d41;
+  display: flex;
+  justify-content: space-between;
+  padding-left: 0 !important;
+  align-items: center;
+  color: #fff;
+  font-size: 20px;
+
+  > div {
+    display: flex;
+    align-items: center;
+
+    span {
+      margin-left: 15px;
+    }
+  }
 }
 
 .el-aside {
-  background-color: #d3dce6;
-  color: #333;
+  background-color: #333744;
   text-align: center;
-  line-height: 200px;
+  .el-menu {
+    border: none;
+  }
 }
 
 .el-main {
-  background-color: #e9eef3;
-  color: #333;
-  text-align: center;
-  line-height: 160px;
+  background-color: #eaedf1;
+  padding: 0;
 }
+
+.iconfont {
+  margin-right: 10px;
+}
+
 </style>
