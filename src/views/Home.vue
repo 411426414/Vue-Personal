23b5790d6bbc3f58faf06b215d237ef99a3eacdd
411426414 <411426414@qq.com>
2020-12-23 23:14:20 +0800

    更新组件

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,65 +1,63 @@
 <template>
   <el-container class="container">
+    <el-header>
+      <div>
+        <img src="@/assets/logo.png" />
+        <span> Header </span>
+      </div>
+      <el-button type="info" @click="logout">退出</el-button>
+    </el-header>
     <el-container>
-      <el-header>
-        <div>
-          <img src="@/assets/logo.png" />
-          <span> Header </span>
-        </div>
-        <el-button type="info" @click="logout">退出</el-button>
-      </el-header>
-      <el-container>
-        <el-aside :width="isCollapse ? '64px' : '200px'">
-          <div class="toggle-button" @click="collapse">|||</div>
-          <el-menu
-            router
-            unique-opened
-            text-color="#fff"
-            :collapse="isCollapse"
-            :collapse-transition="false"
-            class="el-menu-vertical-demo"
-            background-color="#545c64"
-            active-text-color="#ffd04b"
-          >
-            <el-submenu index="1">
-              <template slot="title">
-                <i class="el-icon-location"></i>
-                <span>导航一</span>
-              </template>
-              <el-menu-item-group>
-                <template slot="title">分组一</template>
-                <el-menu-item index="1-1">选项1</el-menu-item>
-                <el-menu-item index="1-2">选项2</el-menu-item>
-              </el-menu-item-group>
-              <el-menu-item-group title="分组2">
-                <el-menu-item index="1-3">选项3</el-menu-item>
-              </el-menu-item-group>
-              <el-submenu index="1-4">
-                <template slot="title">选项4</template>
-                <el-menu-item index="1-4-1">选项1</el-menu-item>
-              </el-submenu>
+      <el-aside :width="isCollapse ? '64px' : '200px'">
+        <div class="toggle-button" @click="collapse">|||</div>
+        <el-menu
+          router
+          unique-opened
+          text-color="#fff"
+          :collapse="isCollapse"
+          :collapse-transition="false"
+          class="el-menu-vertical-demo"
+          background-color="#545c64"
+          active-text-color="#ffd04b"
+        >
+          <el-submenu index="1">
+            <template slot="title">
+              <i class="el-icon-location"></i>
+              <span>导航一</span>
+            </template>
+            <el-menu-item-group>
+              <template slot="title">分组一</template>
+              <el-menu-item index="1-1">选项1</el-menu-item>
+              <el-menu-item index="1-2">选项2</el-menu-item>
+            </el-menu-item-group>
+            <el-menu-item-group title="分组2">
+              <el-menu-item index="1-3">选项3</el-menu-item>
+            </el-menu-item-group>
+            <el-submenu index="1-4">
+              <template slot="title">选项4</template>
+              <el-menu-item index="1-4-1">选项1</el-menu-item>
             </el-submenu>
-            <el-menu-item index="/home">
-              <i class="el-icon-menu"></i>
-              <span slot="title">导航二</span>
-            </el-menu-item>
-            <el-menu-item index="3" disabled>
-              <i class="el-icon-document"></i>
-              <span slot="title">导航三</span>
-            </el-menu-item>
-            <el-menu-item index="/about">
-              <i class="el-icon-setting"></i>
-              <span slot="title">关于</span>
-            </el-menu-item>
-          </el-menu>
-        </el-aside>
-        <el-container>
-          <el-main>
-            <!-- 路由占位符 -->
+          </el-submenu>
+          <el-menu-item index="/home">
+            <i class="el-icon-menu"></i>
+            <span slot="title">导航二</span>
+          </el-menu-item>
+          <el-menu-item index="3" disabled>
+            <i class="el-icon-document"></i>
+            <span slot="title">导航三</span>
+          </el-menu-item>
+          <el-menu-item index="/about">
+            <i class="el-icon-setting"></i>
+            <span slot="title">关于</span>
+          </el-menu-item>
+        </el-menu>
+      </el-aside>
+      <el-container>
+        <el-main>
+          <!-- 路由占位符 -->
           <router-view></router-view>
-          </el-main>
-          <el-footer>Footer</el-footer>
-        </el-container>
+        </el-main>
+        <el-footer>Footer</el-footer>
       </el-container>
     </el-container>
   </el-container>
@@ -78,10 +76,10 @@ export default {
       this.$router.push("/login"); // 跳转到登录页面
     },
     collapse() {
-      this.isCollapse = !this.isCollapse
-    }
-  }
-}
+      this.isCollapse = !this.isCollapse;
+    },
+  },
+};
 </script>
 
 <style lang="less" scoped>
@@ -92,6 +90,7 @@ export default {
 .el-header {
   background-color: #373d41;
   display: flex;
+  // 左右贴边对齐
   justify-content: space-between;
   padding-left: 0 !important;
   align-items: center;
