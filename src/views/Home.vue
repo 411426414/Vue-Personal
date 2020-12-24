d7bda1fa61f03f1c7bb5d0b25d2e98e311086f7c
411426414 <411426414@qq.com>
2020-12-24 23:54:20 +0800

    添加顶部导航条

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,64 +1,41 @@
 <template>
   <el-container class="container">
     <el-header>
-      <div>
-        <img src="@/assets/logo.png" />
-        <span> Header </span>
-      </div>
-      <el-button type="info" @click="logout">退出</el-button>
+      <el-menu
+        router
+        class="el-menu-demo"
+        mode="horizontal"
+        background-color="#545c64"
+        text-color="#fff"
+        active-text-color="#ffd04b"
+      >
+        <el-menu-item index="/home">主页</el-menu-item>
+        <el-menu-item index="/about">关于</el-menu-item>
+        <el-submenu index="2">
+          <template slot="title">我的工作台</template>
+          <el-menu-item index="2-1">选项1</el-menu-item>
+          <el-menu-item index="2-2">选项2</el-menu-item>
+          <el-menu-item index="2-3">选项3</el-menu-item>
+          <el-submenu index="2-4">
+            <template slot="title">选项4</template>
+            <el-menu-item index="2-4-1">选项1</el-menu-item>
+            <el-menu-item index="2-4-2">选项2</el-menu-item>
+            <el-menu-item index="2-4-3">选项3</el-menu-item>
+          </el-submenu>
+        </el-submenu>
+        <el-menu-item index="3" disabled>消息中心</el-menu-item>
+        <el-menu-item index="4">
+          <a href="https://www.ele.me" target="_blank">订单管理</a>
+        </el-menu-item>
+        <el-menu-item>
+          <el-button type="info" @click="logout">退出</el-button>
+        </el-menu-item>
+      </el-menu>
     </el-header>
     <el-container>
-      <el-aside :width="isCollapse ? '64px' : '200px'">
-        <div class="toggle-button" @click="collapse">|||</div>
-        <el-menu
-          router
-          unique-opened
-          text-color="#fff"
-          :collapse="isCollapse"
-          :collapse-transition="false"
-          class="el-menu-vertical-demo"
-          background-color="#545c64"
-          active-text-color="#ffd04b"
-        >
-          <el-submenu index="1">
-            <template slot="title">
-              <i class="el-icon-location"></i>
-              <span>导航一</span>
-            </template>
-            <el-menu-item-group>
-              <template slot="title">分组一</template>
-              <el-menu-item index="1-1">选项1</el-menu-item>
-              <el-menu-item index="1-2">选项2</el-menu-item>
-            </el-menu-item-group>
-            <el-menu-item-group title="分组2">
-              <el-menu-item index="1-3">选项3</el-menu-item>
-            </el-menu-item-group>
-            <el-submenu index="1-4">
-              <template slot="title">选项4</template>
-              <el-menu-item index="1-4-1">选项1</el-menu-item>
-            </el-submenu>
-          </el-submenu>
-          <el-menu-item index="/home">
-            <i class="el-icon-menu"></i>
-            <span slot="title">导航二</span>
-          </el-menu-item>
-          <el-menu-item index="3" disabled>
-            <i class="el-icon-document"></i>
-            <span slot="title">导航三</span>
-          </el-menu-item>
-          <el-menu-item index="/about">
-            <i class="el-icon-setting"></i>
-            <span slot="title">关于</span>
-          </el-menu-item>
-        </el-menu>
-      </el-aside>
-      <el-container>
-        <el-main>
-          <!-- 路由占位符 -->
-          <router-view></router-view>
-        </el-main>
-        <el-footer>Footer</el-footer>
-      </el-container>
+      <el-main>
+      </el-main>
+      <el-footer>Footer</el-footer>
     </el-container>
   </el-container>
 </template>
