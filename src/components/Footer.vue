e572c5697a4d9052f2fe83fdd0a908f8e5d3c20a
411426414 <411426414@qq.com>
2020-12-25 22:44:14 +0800

    定义底部框架及基本样式

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,12 +1,67 @@
 <template>
-  <div></div>
+  <div class="container">
+    <div class="link-wrap">
+      <div class="link-item">
+        <h4>Navigation</h4>
+        <ul>
+          <li><a href="">Home</a></li>
+          <li><a href="">Design</a></li>
+          <li><a href="">Blog</a></li>
+          <li><a href="">About Me</a></li>
+          <li><a href="">给我留言</a></li>
+        </ul>
+      </div>
+      <div class="link-item">
+        <h4>Follow Me</h4>
+        <ul>
+          <li><a href="">Weibo</a></li>
+          <li><a href="">Wechat</a></li>
+          <li><a href="">QQ</a></li>
+          <li><a href="">Email</a></li>
+          <li><a href="">CSDN</a></li>
+        </ul>
+      </div>
+      <div class="link-item">
+        <h4>Blog Website</h4>
+        <ul>
+          <li><a href="">CSDN</a></li>
+          <li><a href="">OSCHINA</a></li>
+          <li><a href="">segmentfault</a></li>
+          <li><a href="">juejin</a></li>
+          <li><a href="">给我留言</a></li>
+        </ul>
+      </div>
+      <div class="link-item">
+        <h4>Resources</h4>
+        <ul>
+          <li><a href="">Designmodo</a></li>
+          <li><a href="">Tympanus.net</a></li>
+          <li><a href="">Uisdc</a></li>
+          <li><a href="">scotch.io</a></li>
+          <li><a href="">css-tricks</a></li>
+        </ul>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
-  name: "Footer"
+  name: "Footer",
 };
 </script>
 
-<style lang="scss" scoped>
+<style lang="less" scoped>
+.footer {
+  width: 100%;
+  height: 312px;
+  position: relative;
+  margin: 4rem 0 0;
+  padding: 3rem 15px;
+  font-size: 12px;
+  line-height: 1.75rem;
+  background: #fff;
+  color: rgba(0, 0, 0, 0.5);
+  font-weight: 400;
+}
 </style>
