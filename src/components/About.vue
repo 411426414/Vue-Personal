5915ed0253a9aef1beaf448dc9e0d05a260833fc
411426414 <411426414@qq.com>
2020-12-27 22:57:08 +0800

    设置header样式

--- src/components/About.vue
+++ src/components/About.vue
@@ -41,7 +41,12 @@ export default {};
 
 <style lang="less" scoped>
 .header {
+  // background: url(https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1608574050504&di=68f515bd3d85c220d756640c725fb8e2&imgtype=0&src=http%3A%2F%2Fbbsfiles.vivo.com.cn%2Fvivobbs%2Fattachment%2Fforum%2F201706%2F24%2F120513xjmo2xki377j4d9x.jpg);
+  position: relative;
   height: 100vh;
+  background: url(https://www.dengzhanyong.com/img/aboutMe-bg.fc46291b.jpg) no-repeat bottom;
+  background-size: cover;
+  background-attachment: fixed;
 }
 .about-me {
   max-width: 960px;
