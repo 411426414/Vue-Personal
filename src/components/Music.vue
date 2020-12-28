a7eb17bb319be502e1819734fc174d042b16c82c
411426414 <411426414@qq.com>
2020-12-28 23:02:26 +0800

    设置top背景

--- src/components/Music.vue
+++ src/components/Music.vue
@@ -1,10 +1,25 @@
 <template>
-  <div>音乐</div>
+  <div>
+    <div class="m-top">
+    </div>
+  </div>
 </template>
 
 <script>
 export default {};
 </script>
 
-<style lang="scss" scoped>
+<style lang="less" scoped>
+* {
+  margin: 0;
+  padding: 0;
+}
+.m-top {
+  position: relative;
+  z-index: 1000;
+  height: 70px;
+  box-sizing: border-box;
+  background: #242424;
+  border-bottom: 1px solid #000;
+}
 </style>
