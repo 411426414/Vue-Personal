35edac9feebaec5bfb775ba09181a2c68224b198
411426414 <411426414@qq.com>
2020-12-31 23:42:40 +0800

    把QQ音乐界面设置为App的背景

--- src/App.vue
+++ src/App.vue
@@ -10,5 +10,14 @@
 	}
 </script>
 
-<style>
+<style lang="less">
+#app {
+  width: 100vw;
+  height: 100vh;
+  display: block;
+  position: absolute;
+  top: 0;
+  left: 0;
+  background: url("./assets/QQ截图20201231232338.png")
+}
 </style>
