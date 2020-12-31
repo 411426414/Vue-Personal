f72d328f3e7028616a5ede8e5b3a5d4c800fabdb
411426414 <411426414@qq.com>
2020-12-31 23:30:14 +0800

    QQ组件

---
+++ src/views/qqMusic.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="bg">
+12345y
+  </div>
+</template>
+
+<script>
+export default {
+name: "qqMusic"
+}
+</script>
+
+<style lang="less" scoped>
+.bg {
+  width: 100vw;
+  height: 100vh;
+  background: url("../assets/QQ截图20201231232338.png")
+}
+</style>
