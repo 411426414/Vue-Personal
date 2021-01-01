0f66ade3b45a9575917c800833665014147a4e22
411426414 <411426414@qq.com>
2021-01-01 22:19:39 +0800

    重置标签的边距

--- src/views/qqMusic.vue
+++ src/views/qqMusic.vue
@@ -32,12 +32,14 @@ export default {
 </script>
 
 <style lang="less" scoped>
+* {
+  margin: 0;
+  padding: 0;
+}
 body {
   font-family: Helvetica Neue, Helvetica, PingFang SC, Hiragino Sans GB,
     Microsoft YaHei, "\5FAE\8F6F\96C5\9ED1", Arial, sans-serif;
   height: 100%;
-  margin: 0;
-  padding: 0;
   min-width: 1366px;
 }
 li {
