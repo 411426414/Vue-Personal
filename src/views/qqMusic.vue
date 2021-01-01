86ea28bfb2d6a8b9f8497d362d41dae82c0defda
411426414 <411426414@qq.com>
2021-01-01 22:18:28 +0800

    设置导航的样式及超链接样式

--- src/views/qqMusic.vue
+++ src/views/qqMusic.vue
@@ -11,6 +11,13 @@
               />
             </a>
           </h1>
+          <ul class="mod_top_nav">
+            <li><a class="active" href="">音乐馆</a></li>
+            <li><a href="">我的音乐</a></li>
+            <li><a href="">客户端</a></li>
+            <li><a href="">开放平台</a></li>
+            <li><a href="">VIP</a></li>
+          </ul>
         </div>
       </div>
     </el-header>
@@ -56,6 +63,28 @@ a {
         .qqmusic_logo {
           width: 170px;
           height: 46px;
+        }
+      }
+      .mod_top_nav {
+        position: absolute;
+        left: 198px;
+        top: 0;
+        li {
+          position: relative;
+          float: left;
+          margin-right: -5px;
+          a {
+            display: block;
+            padding: 0 20px;
+            line-height: 90px;
+            height: 90px;
+            text-align: center;
+            font-size: 18px;
+          }
+          a.active {
+            background-color: #31c27c;
+            color: #fff;
+          }
         }
       }
     }
