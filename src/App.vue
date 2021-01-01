ec243bcbe7642ce70d59f6ce5a2878dcdaa1061f
411426414 <411426414@qq.com>
2021-01-01 21:10:47 +0800

    更新App组件

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,13 @@
 <template>
-	<div id="app">
-		<router-view></router-view>
-	</div>
+  <div id="app">
+    <router-view></router-view>
+  </div>
 </template>
 
 <script>
-	export default {
-		name: 'app'
-	}
+export default {
+  name: "app",
+};
 </script>
 
-<style lang="less">
-#app {
-  width: 100vw;
-  height: 100vh;
-  display: block;
-  position: absolute;
-  top: 0;
-  left: 0;
-  background: url("./assets/QQ截图20201231232338.png")
-}
-</style>
+<style lang="less"></style>
