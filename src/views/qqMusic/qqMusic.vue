8c5347d5465f78b106725a6a4cc2927a0b793490
411426414 <411426414@qq.com>
2021-01-02 23:46:47 +0800

    添加背景样式

---
+++ src/views/qqMusic/qqMusic.vue
@@ -0,0 +1,19 @@
+<template>
+  <el-container>
+    <el-main class="mod_bg">Main</el-main>
+  </el-container>
+</template>
+
+<script>
+export default {
+  name: "qqMusic"
+};
+</script>
+
+<style lang="less" scoped>
+.mod_bg {
+  background: url(https://y.gtimg.cn/mediastyle/yqq/img/bg_detail.jpg) 50% 0
+    repeat-x;
+  position: relative;
+}
+</style>
