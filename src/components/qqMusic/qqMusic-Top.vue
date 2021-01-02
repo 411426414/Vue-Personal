a3a4649f9df0483f5719a5b65c93f9159283127f
411426414 <411426414@qq.com>
2021-01-02 23:44:33 +0800

    添加logo图片

---
+++ src/components/qqMusic/qqMusic-Top.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="nav-top">
+    <h1 class="qqmusic_title">
+      <a href="" class="qqmusic_title">
+        <img
+          class="qqmusic_logo"
+          src="https://y.gtimg.cn/mediastyle/yqq/img/logo@2x.png"
+        />
+      </a>
+    </h1>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="less" scoped>
+.nav-top {
+  height: 90px;
+  .qqmusic_title {
+    position: absolute;
+    left: 0;
+    top: 10px;
+    .qqmusic_logo {
+      width: 170px;
+      height: 46px;
+    }
+  }
+}
+</style>
