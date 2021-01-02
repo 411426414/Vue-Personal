612fa2c4baa03981c836e583aa1f3f536d193c5b
411426414 <411426414@qq.com>
2021-01-02 23:47:15 +0800

    引入头部导航

--- src/views/qqMusic/qqMusic.vue
+++ src/views/qqMusic/qqMusic.vue
@@ -1,16 +1,34 @@
 <template>
   <el-container>
+    <el-header class="main">
+      <div class="section_inner">
+        <qqMusicTop></qqMusicTop>
+      </div>
+    </el-header>
     <el-main class="mod_bg">Main</el-main>
   </el-container>
 </template>
 
 <script>
+import qqMusicTop from "@/components/qqMusic/qqMusic-Top";
 export default {
-  name: "qqMusic"
+  name: "qqMusic",
+  components: {
+    qqMusicTop
+  },
 };
 </script>
 
 <style lang="less" scoped>
+.main {
+  height: 100% !important;
+  .section_inner {
+    background: #fff;
+    max-width: 1200px;
+    margin: 0 auto;
+    position: relative;
+  }
+}
 .mod_bg {
   background: url(https://y.gtimg.cn/mediastyle/yqq/img/bg_detail.jpg) 50% 0
     repeat-x;
