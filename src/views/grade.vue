a10a618fda5aaf797037078438c2aa7e452854d8
411426414 <411426414@qq.com>
2021-01-03 19:02:15 +0800

    添加成绩计算的组件

---
+++ src/views/grade.vue
@@ -0,0 +1,17 @@
+<template>
+  <el-card>
+    成绩
+  </el-card>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="less" scoped>
+.el-card {
+  width: 60%;
+  margin: 0 auto;
+  box-shadow: 0 1px 1px rgba(0, 0, 0, 0.15);
+}
+</style>
