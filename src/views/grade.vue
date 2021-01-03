e7e313087078691c3e3d940d358434522e4de52b
411426414 <411426414@qq.com>
2021-01-03 19:02:39 +0800

    引入table表格

--- src/views/grade.vue
+++ src/views/grade.vue
@@ -1,11 +1,31 @@
 <template>
   <el-card>
-    成绩
+    <el-table :data="TableData" border show-summary style="width: 100%">
+      <el-table-column width="100" type="index" label="科目"></el-table-column>
+      <el-table-column prop="Grade" label="该科成绩"></el-table-column>
+      <el-table-column prop="GetCredits" label="取得学分"></el-table-column>
+      <el-table-column prop="GradePoint" label="绩点"></el-table-column>
+      <el-table-column prop="CreditGradePoint" label="学分绩点">
+      </el-table-column>
+    </el-table>
   </el-card>
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      TableData: [
+        {
+          Grade: "",
+          GetCredits: "",
+          GradePoint: "",
+          CreditGradePoint: "",
+        },
+      ],
+    };
+  },
+};
 </script>
 
 <style lang="less" scoped>
