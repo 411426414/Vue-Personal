6f80acb94d88e84753661d8da297846840c2d7c5
411426414 <411426414@qq.com>
2021-01-03 19:05:11 +0800

    引入dialog对话框,使用添加科目按钮显示

--- src/views/grade.vue
+++ src/views/grade.vue
@@ -1,6 +1,6 @@
 <template>
   <el-card>
-    <el-button type="primary">
+    <el-button type="primary" @click="gradeDialogVisible = true">
       <h1>添加科目</h1>
     </el-button>
     <el-table :data="TableData" border show-summary style="width: 100%">
@@ -11,6 +11,19 @@
       <el-table-column prop="CreditGradePoint" label="学分绩点">
       </el-table-column>
     </el-table>
+    <el-dialog
+      title="将每学科对应成绩及学分输入到表格中对应处"
+      :visible.sync="gradeDialogVisible"
+      width="50%"
+    >
+      <span> 提示 </span>
+      <span slot="footer" class="dialog-footer">
+        <el-button @click="gradeDialogVisible = false">取 消</el-button>
+        <el-button type="primary" @click="gradeDialogVisible = false">
+          确 定
+        </el-button>
+      </span>
+    </el-dialog>
   </el-card>
 </template>
 
@@ -18,6 +31,7 @@
 export default {
   data() {
     return {
+      gradeDialogVisible: false,
       TableData: [
         {
           Grade: "",
