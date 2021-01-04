275c54a7cccf90bc4425809280f617cafbce8967
411426414 <411426414@qq.com>
2021-01-04 20:34:01 +0800

    更新form对象的数据项

--- src/views/grade.vue
+++ src/views/grade.vue
@@ -41,8 +41,6 @@ export default {
       form: {
         Grade: "",
         GetCredits: "",
-        GradePoint: "",
-        CreditGradePoint: "",
       },
       gradeDialogVisible: false,
       TableData: [],
