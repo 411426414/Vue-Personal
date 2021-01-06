8b437d7daf391c29434328e7a05263ebc7c568a3
411426414 <411426414@qq.com>
2021-01-06 23:21:06 +0800

    添加列表表题

--- src/components/qqMusic/List.vue
+++ src/components/qqMusic/List.vue
@@ -1,9 +1,18 @@
 <template>
-  <div>歌曲列表</div>
+  <div>
+    <ul class="title"></ul>
+  </div>
 </template>
 
 <script>
 export default {};
 </script>
 
-<style lang="less" scoped></style>
+<style lang="less" scoped>
+.title {
+  background: #d9edf7;
+  width: 1200px;
+  height: 50px;
+  margin: 0 auto;
+}
+</style>
