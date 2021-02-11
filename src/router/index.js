4ff6e085337cc1134ee91653ee5e8ff6c3f71da4
411426414 <411426414@qq.com>
2021-02-11 18:14:56 +0800

    添加home组件路由

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,14 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Login from '../components/Login.vue'
 
 Vue.use(VueRouter)
 
 const routes = [
   {
-    path: '/',
-    name: 'Login',
-    component: Login
-  }
+    path: '/home',
+    name: 'Home',
+    component: () => import('@/views/Home'),
+  },
 ]
 
 const router = new VueRouter({
