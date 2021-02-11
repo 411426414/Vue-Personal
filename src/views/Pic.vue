dacfb6b47e46fa89dba9ac8a7f5958f8cdc8050e
411426414 <411426414@qq.com>
2021-02-11 18:35:00 +0800

    引入Picitem组件

--- src/views/Pic.vue
+++ src/views/Pic.vue
@@ -1,20 +1,4 @@
 <template>
-<<<<<<< HEAD
-  <div>图片</div>
-</template>
-
-<script>
-export default {
-  data() {
-    return {
-      key: 'value'
-    }
-  },
-  methods: {
-    name() {
-      
-    }
-=======
   <div>
     <PicItem></PicItem>
   </div>
@@ -25,7 +9,6 @@ import PicItem from "../components/PicItem";
 export default {
   components: {
     PicItem,
->>>>>>> Pic
   },
 };
 </script>
